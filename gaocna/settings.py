ed381465c0a81b7737b2bf56586511957b845068
AndriaBeridze <andria24b@gmail.com>
2024-10-12 22:29:55 -0400

    Temporarily allow all hosts

--- gaocna/settings.py
+++ gaocna/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-bjc4^&_7$zn9=b1+^hs=j@%r9x*fvw21l5nh5ayxt+#&5vte8e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.0.214']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
